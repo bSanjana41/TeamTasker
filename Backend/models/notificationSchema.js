489b64e649e547fb02a2043d83f57488297ac8c6
bSanjana41 <bsanjana41@gmail.com>
2025-09-26 18:36:40 +0530

    Update notificationSchema to include 'updated' type in notification types

--- Backend/models/notificationSchema.js
+++ Backend/models/notificationSchema.js
@@ -5,7 +5,7 @@ const { Schema, model, models, SchemaTypes } = mongoose;
 const notificationSchema = new Schema({
     user: { type: SchemaTypes.ObjectId, ref: "User", required: true },
     task: { type: SchemaTypes.ObjectId, ref: "Task", required: true },
-    type: { type: String, enum: ["assigned", "commented"], default: "commented" },
+    type: { type: String, enum: ["assigned","updated","commented"], default: "commented" },
     text: { type: String, required: true },
     read: {type:Boolean,default:false},
 },
